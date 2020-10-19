67af80974f3c03dfef0eb018a235ef0cd5e135f3
Karlo <seles.karlo@gmail.com>
2020-10-19 23:03:18 +0200

    fetch movie API

---
+++ src/services/movieAPI.js
@@ -0,0 +1,15 @@
+class MovieAPI {
+  baseURL = "https://api.themoviedb.org/3/movie/";
+  apiKey = "d2c77997d82f36d98cfeab0c8a259de7";
+
+  async getMovies(page) {
+    const response = await fetch(
+      `${this.baseURL}popular?api_key=${this.apiKey}&page=${page}`
+    );
+    const data = await response.json();
+
+    return data;
+  }
+}
+
+export default new MovieAPI();
