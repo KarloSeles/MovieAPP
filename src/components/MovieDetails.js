614c0518c8e625597d7f133fa099fe1b9bc7bbf6
Karlo <seles.karlo@gmail.com>
2020-10-24 01:25:45 +0200

    created details page

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import movieAPI from "../services/movieAPI";
 
+function getMovieYear(movieDate) {
+  const year = new Date(movieDate);
+  return year.getFullYear();
+}
+
 function MovieDetails(props) {
   const [id, setId] = useState(null);
 
@@ -11,10 +16,29 @@ function MovieDetails(props) {
     };
     getId();
   }, []);
+
   if (!id) {
     return <div>loading...</div>;
   }
-  return <div>{id.title}</div>;
+  return (
+    <div className="details-container">
+      <button onClick={props.onClick} className="close-btn">
+        X
+      </button>
+      <p className="details-name">
+        {id.title} ({getMovieYear(id.release_date)})
+      </p>
+      <img
+        className="details-img"
+        src={`https://image.tmdb.org/t/p/original${id.backdrop_path}`}
+      />
+      <h4 className="details-overview">{id.overview}</h4>
+      <h3 className="detail-rating">Rating: {id.vote_average}</h3>
+      <h3 className="detail-popularity">Popularity: {id.vote_count}</h3>
+      <h3 className="detail-language">Language: {id.original_language}</h3>
+      <h3 className="detail-production">Production companies:</h3>
+    </div>
+  );
 }
 
 export default MovieDetails;
