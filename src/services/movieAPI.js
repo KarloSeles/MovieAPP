a7565870672108a0d1eb05cd6aed2b948e7f900a
Karlo <seles.karlo@gmail.com>
2020-10-24 22:26:30 +0200

    Update movieAPI.js

--- src/services/movieAPI.js
+++ src/services/movieAPI.js
@@ -17,6 +17,30 @@ class MovieAPI {
 
     return data;
   }
+
+  async requestToken() {
+    const response = await fetch(`
+    https://api.themoviedb.org/3/authentication/token/new?api_key=${this.apiKey}`);
+    const data = await response.json();
+
+    return data;
+  }
+
+  async getSession(token) {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ request_token: token }),
+    };
+    const response = await fetch(
+      `
+    https://api.themoviedb.org/3/authentication/session/new?api_key=${this.apiKey}`,
+      requestOptions
+    );
+    const data = await response.json();
+
+    return data;
+  }
 }
 
 export default new MovieAPI();
