bd540f019d1f633951008c5849c76748aa84020e
Karlo <seles.karlo@gmail.com>
2020-11-02 17:23:59 +0100

    Update LogIn.js

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,23 +1,71 @@
 import React, { useEffect, useState } from "react";
 import movieAPI from "../services/movieAPI";
+import Cookies from "js-cookie";
 
 function LogIn() {
-  const [token, setToken] = useState();
+  const [session, setSession] = useState(Cookies.get("session"));
+  const [token, setToken] = useState(Cookies.get("token"));
+  const [pendingToken, setPendingToken] = useState(null);
 
   useEffect(() => {
-    const getToken = async () => {
-      const token = await movieAPI.requestToken();
-      setToken(token.request_token);
-    };
-    getToken();
+    const params = new URL(window.location.href).searchParams;
+    const checkToken = params.get("request_token");
+    if (checkToken) {
+      setToken(checkToken);
+    }
   }, []);
+
+  useEffect(() => {
+    if (session) {
+      Cookies.set("session", session, { expires: 7 });
+    } else {
+      Cookies.remove("session");
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (token) {
+      Cookies.set("token", token, { expires: 7 });
+    } else {
+      Cookies.remove("token");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (!token) {
+      const getToken = async () => {
+        const response = await movieAPI.requestToken();
+        setPendingToken(response.request_token);
+      };
+      getToken();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!session && token) {
+      const getSession = async () => {
+        const response = await movieAPI.getSession(token);
+        setSession(response.session_id);
+      };
+      getSession();
+    }
+  }, [token]);
+
+  function clearState() {
+    setSession();
+    setToken();
+  }
 
   return (
     <div>
       <a
-        href={`https://www.themoviedb.org/authenticate/${token}?redirect_to=http://localhost:3000/approved`}
+        href={`https://www.themoviedb.org/authenticate/${pendingToken}?redirect_to=http://localhost:3000//approved`}
       >
-        Log in
+        Log In
+      </a>
+
+      <a href="http://localhost:3000" onClick={clearState}>
+        Log Out
       </a>
     </div>
   );
