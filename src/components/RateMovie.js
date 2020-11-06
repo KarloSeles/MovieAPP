25b4953b4eed3307f70937682ae138f2f7b8c95a
Karlo <seles.karlo@gmail.com>
2020-11-06 21:12:41 +0100

    Update RateMovie.js

--- src/components/RateMovie.js
+++ src/components/RateMovie.js
@@ -1,38 +1,71 @@
 import React, { useEffect, useState } from "react";
 import movieAPI from "../services/movieAPI";
+import Cookies from "js-cookie";
 
 function RateMovie(props) {
-  const [token, setToken] = useState();
-  const [session, setSession] = useState();
+  const [value, setValue] = useState();
+  const [loading, setLoading] = useState(true);
+  const sessionValue = Cookies.get("session");
 
   useEffect(() => {
-    const getToken = async () => {
-      const token = await movieAPI.requestToken();
-      setToken(token.request_token);
-    };
-    getToken();
-  }, []);
+    if (value && !loading) {
+      const rate = async () => {
+        await movieAPI.rateMovie(props.id, sessionValue, value);
+      };
+
+      rate();
+    }
+  }, [value]);
 
   useEffect(() => {
-    const getSessionId = async () => {
-      const session = await movieAPI.getSession(token);
-      setSession(session.session_id);
-    };
-    getSessionId();
+    async function getRating() {
+      const response = await movieAPI.getUserRating(props.id, sessionValue);
+      setValue(response.rated.value);
+      setLoading(false);
+    }
+    if (sessionValue) {
+      getRating();
+    }
   }, []);
+
+  function rateValue(ratingValue) {
+    if (sessionValue) {
+      setValue(ratingValue);
+    }
+  }
 
   return (
-    <div>
-      <div>X</div>
-      <div>X</div>
-      <div>X</div>
-      <div>X</div>
-      <div>X</div>
-      <div>X</div>
-      <div>X</div>
-      <div>X</div>
-      <div>X</div>
-      <div>X</div>
+    <div className={"rating-" + value}>
+      <button className="rating-btn" onClick={() => rateValue(1)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(2)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(3)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(4)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(5)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(6)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(7)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(8)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(9)}>
+        X
+      </button>
+      <button className="rating-btn" onClick={() => rateValue(10)}>
+        X
+      </button>
     </div>
   );
 }
